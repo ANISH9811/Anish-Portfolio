5978cf519bd5292abb5492f3acf0b3f7b3af0eb8
Anish <pathakanish9811@gmail.com>
2023-02-13 23:12:38 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require("./app");
+// const dotenv = require("dotenv");
+// dotenv.config({ path: "./config.env" });
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connection established"));
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("listening");
+});
